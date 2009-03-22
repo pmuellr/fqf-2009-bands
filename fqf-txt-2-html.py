ad0982040043be3f3d033f7172c15ef68457a8d7
Patrick Mueller <pmuellr@yahoo.com>
2009-03-21 23:34:18 -0400

    making progress on python html generator

--- fqf-txt-2-html.py
+++ fqf-txt-2-html.py
@@ -3,8 +3,168 @@
 import os
 import sys
 import re
+import StringIO
 
-ifile_name = os.path.expanduser("~/Documents/2009-fqf-bands.txt")
+#--------------------------------------------------------------------
+#  globals
+#--------------------------------------------------------------------
+time_start = 11
+time_stop  =  9
+
+#--------------------------------------------------------------------
+#
+#--------------------------------------------------------------------
+def hour_to_print(hour):
+	hour = hour % 12
+	if not hour: hour = 12
+	return hour
+
+#--------------------------------------------------------------------
+#
+#--------------------------------------------------------------------
+def time_to_units(hh, mm):
+	return ((hh - time_start) * 12 ) + (mm / 5)
+		
+#--------------------------------------------------------------------
+#
+#--------------------------------------------------------------------
+class Day:
+
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def __init__(self, name):
+		self.name    = name
+		self.blocks  = []
+	
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def add_block(self, block):
+		self.blocks.append(block)
+		return self
+		
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def get_blocks(self):
+		return self.blocks[:]
+		
+#--------------------------------------------------------------------
+#
+#--------------------------------------------------------------------
+class Block:
+
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def __init__(self, name, color):
+		self.name    = name
+		self.color   = color
+		self.entries = []
+
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def add_entry(self, entry):
+		self.entries.append(entry)
+		return self
+		
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def get_entries(self):
+		return self.entries[:]
+		
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def to_html_tr(self, ofile):
+		print >>ofile, "<tr style='background-color: #%s'>" % self.color
+		print >>ofile, "<td class='table_header'>%s</td><td class='clear'></td>" % self.name
+		
+		entries = self.get_entries()
+		
+		if entries[0].units_start != 0:
+			cols = entries[0].units_start
+			print >> ofile, "<td colspan='%d' class='clear'></td>" % cols
+		
+		
+		for i, entry in enumerate(entries):
+			cols = entry.units_stop - entry.units_start
+			print >>ofile, "<td colspan='%d'>" % cols,
+			entry.to_html(ofile)
+			print >>ofile, "</td>"
+
+			if i < len(entries) - 1:
+				units_empty = entries[i+1].units_start - entry.units_stop
+				if units_empty > 0:
+					print >>ofile, "<td colspan='%d' class='clear'></td>" % units_empty
+			
+		print >>ofile, "</tr>"
+		
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def to_html(self, ofile):
+	
+		print >>ofile, "<div style='background-color: #%s'>" % self.color
+		print >>ofile, "<h2>%s</h2>" % self.name
+		
+		for entry in self.get_entries():
+			entry.to_html(ofile)
+			print >>ofile, ""
+			
+		print >>ofile, "</div>"
+		print >>ofile
+
+#--------------------------------------------------------------------
+#
+#--------------------------------------------------------------------
+class Entry:
+
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def __init__(self, hh1, mm1, hh2, mm2, name, link):
+	
+		self.hh1  = int(hh1.strip())
+		self.mm1  = int(mm1.strip())
+		self.hh2  = int(hh2.strip())
+		self.mm2  = int(mm2.strip())
+		self.name = name.strip()
+		self.link = link
+		
+		if self.hh1 < 10: self.hh1 += 12
+		if self.hh2 < 10: self.hh2 += 12
+		
+		self.units_start  = time_to_units(self.hh1, self.mm1)
+		self.units_stop   = time_to_units(self.hh2, self.mm2)
+		self.units_length = self.units_stop - self.units_start 
+		
+		if self.link: self.link = self.link.strip()
+		
+	#----------------------------------------------------------------
+	#
+	#----------------------------------------------------------------
+	def to_html(self, ofile):
+		if self.link:
+			print >>ofile, "<a href='%s'>%s</a>" % (self.link, self.name),
+			return
+			
+		print >>ofile, self.name,
+
+#--------------------------------------------------------------------
+#
+#--------------------------------------------------------------------
+units_stop = time_to_units(9,0)
+		
+#--------------------------------------------------------------------
+# read the input file
+#--------------------------------------------------------------------
+ifile_name = "2009-fqf-bands.txt"
+ofile_name = "2009-fqf-bands.html"
+
 ifile = open(ifile_name, "r")
 lines = ifile.readlines()
 ifile.close()
@@ -15,47 +175,108 @@ re_color = re.compile(r"^\*color/s*(/S{3})$")
 re_color = re.compile(r"^\*color(.*)$")
 
 color    = "FFF"
-in_block = False
-
+days = []
+day   = None
+block = None
 for line in lines:
 	line = line.rstrip()
-	
+
+	# empty line	
 	if line == "": 
-		if in_block:
-			in_block = False
-			print "</div>"
-			print
-			continue
-		
-		print line
+		block = None
 		continue
 
-	if re_day.match(line):
-		print "<h1>%s</h1>" % line
-		continue
-
+	# color command
 	match = re_color.match(line)
 	if match:
 		color = match.group(1).strip()
 		continue
 		
-	match = re_entry.match(line)
-	if match:
-		time1 = "%s:%s" % (match.group(1), match.group(2))
-		time2 = "%s:%s" % (match.group(3), match.group(4))
-		name  = match.group(5).strip()
-		url   = match.group(6)
-		
-		if url:
-			print "%s to %s <a href='%s'>%s</a><br>" % (time1, time2, url, name)
-		else:
-			print "%s to %s %s<br>" % (time1, time2, name)
-		
+	# a new day
+	if re_day.match(line):
+		day = Day(line)
+		days.append(day)
 		continue
 
-	in_block = True		
-	print "<div style='background-color: #%s'>" % color
-	print "<h2>%s</h2>" % line
+	# a new block
+	match = re_entry.match(line)
+	if not match:
+		block = Block(line, color)
+		day.add_block(block)
+		continue
+	
+	# a new entry
+	hh1  = match.group(1)
+	mm1  = match.group(2)
+	hh2  = match.group(3)
+	mm2  = match.group(4)
+	name = match.group(5)
+	link = match.group(6)
+	
+	entry = Entry(hh1, mm1, hh2, mm2, name, link)
+	block.add_entry(entry)
+		
+#--------------------------------------------------------------------
+# write the output
+#--------------------------------------------------------------------
+ofile = open(ofile_name, "w")
 
-if in_block:
-	print "</div>"
+# things run from 11 - 9, 10 total hours
+# 5 minute increments, 12 increments per hour
+# 10 hours * 12 increments per hour = 120 total increments
+
+blocks_per_table = 8
+
+html_header = """
+<html>
+<head>
+<title>2009 French Quarter Festival Bands</title>
+<style type="text/css">
+.table_header {
+	font-weight: bold;
+	font-size:   120%;
+}
+.clear {
+   background-color: #FFF;
+}
+</style>
+</head>
+<body>
+<h1>2009 French Quarter Festival Bands</h1>
+"""
+
+html_trailer = """
+</body>
+</html>
+"""
+
+print >>ofile, html_header
+
+for day in days:
+	print >>ofile, "<div>"
+	print >>ofile, "<h1>%s</h1>" % day.name
+	print >>ofile, "<table cellpadding='2' cellspacing='0'>"
+	print >>ofile, ""
+	
+	size_line = "<tr><td width='20%'></td>"
+	for i in xrange(0, 120):
+		size_line += "<td width='0.8%'></td>"
+	size_line += "</tr>"
+	
+	date_line = "<tr><td width='20%'></td>"
+	for i in xrange(time_start, time_stop + 12):
+		date_line += "<td width='8%%'colspan='12'>%d</td>" % hour_to_print(i)
+	date_line += "</tr>"
+	
+	print >>ofile, size_line
+	blocks = day.get_blocks()
+	for i, block  in  enumerate(blocks):
+		if not i % 4: print >>ofile, date_line
+		block.to_html_tr(ofile)
+		
+	print >>ofile, "</table>"
+	print >>ofile, "</div>"
+
+print >>ofile, html_trailer
+		
+ofile.close()
